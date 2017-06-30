ca4ef5d6d6a038e5bfc6795eab040b51b7ca2244
worawutp <worawutp@cppc.co.th>
2017-07-01 00:13:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -50,7 +50,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => $messages,
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
