60353ef9f9ebd6e7b78841c63c6e4f837deb633a
worawutp <worawutp@cppc.co.th>
2017-07-01 00:31:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,10 +1,6 @@
 <?php
-require_once '../vendor/autoload.php';
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-use Monolog\Handler\FirePHPHandler;
-$logger = new Logger('LineBot');
-$logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
+
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('ycsRyUP3IrJCgcbST29BS8a/tVy8M1r9+OS/ABuVM7C/OBSr7Xu7UeZ4Th+4sQrY1MvE+KWCtaCOe5AKSqkmm1tMfStK+enOTj1Q+xL4KkUMLGWRwQoGm85pc+uDWG2CzJFmxJegeHugpcLE89ypKAdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'b96d48f95b963b9d3c9a4f138a4c72f0']);
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
@@ -20,6 +16,6 @@ try {
   error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
 }
 foreach ($events as $event) {
-  $outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("text message");
+  $outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('text message');
 $bot->replyMessage($event->getReplyToken(), $outputText);
 }  
